e0c49844df3990541f55f6b4c409931cb2aa4fb8
askar76-r@yandex.ru <askar76-r@yandex.ru>
2020-08-07 11:04:07 +0600

    second commit

--- myshop/orders/migrations/0001_initial.py
+++ myshop/orders/migrations/0001_initial.py
@@ -39,7 +39,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('price', models.DecimalField(decimal_places=2, max_digits=10)),
                 ('quantity', models.PositiveIntegerField(default=1)),
-                ('order', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='items', to='orders.Order')),
+                ('order', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='items', to='order.Order')),
                 ('product', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='order_items', to='shop.Product')),
             ],
         ),
--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -15,7 +15,7 @@ def product_list(request, category_slug=None):
     products = products.filter(category=category)
 
   cart = Cart(request)
-  return render(request, 'shop/product/list.html',
+  return render(request, 'product/list.html',
                           {'category': category,
                            'categories': categories,
                            'products': products,
@@ -30,7 +30,7 @@ def product_detail(request, id, slug):
   cart_product_form = CartAddProductForm( )
 
   cart = Cart(request)
-  return render(request, 'shop/product/detail.html',
+  return render(request, 'product/detail.html',
                 context={'product': product,
                          'cart_product_form':cart_product_form, #устанав. в форме select
                          'cart': cart
